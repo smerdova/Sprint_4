7b60d1a870e102b4767aac96e73cb8a2a8ffe12a
Nataliya Smerdova <smerdova.nataliya@yandex.ru>
2024-04-30 12:11:48 +0300

    Исправление по ревью

--- src/test/java/ImportantQuestionsTest.java
+++ src/test/java/ImportantQuestionsTest.java
@@ -20,34 +20,82 @@ public class ImportantQuestionsTest {
         return Arrays.asList(
                 new Object[][] {
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Сколько это стоит? И как оплатить?",
                                 "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Хочу сразу несколько самокатов! Так можно?",
                                 "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Как рассчитывается время аренды?",
                                 "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Можно ли заказать самокат прямо на сегодня?",
                                 "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Можно ли продлить заказ или вернуть самокат раньше?",
                                 "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Вы привозите зарядку вместе с самокатом?",
                                 "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
                                 "Можно ли отменить заказ?",
                                 "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
                         },
                         {
+                                WebDriverFactory.BrowserType.FIREFOX,
+                                "Я жизу за МКАДом, привезёте?",
+                                "Да, обязательно. Всем самокатов! И Москве, и Московской области.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Сколько это стоит? И как оплатить?",
+                                "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Хочу сразу несколько самокатов! Так можно?",
+                                "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Как рассчитывается время аренды?",
+                                "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Можно ли заказать самокат прямо на сегодня?",
+                                "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Можно ли продлить заказ или вернуть самокат раньше?",
+                                "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Вы привозите зарядку вместе с самокатом?",
+                                "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
+                                "Можно ли отменить заказ?",
+                                "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
+                        },
+                        {
+                                WebDriverFactory.BrowserType.CHROME,
                                 "Я жизу за МКАДом, привезёте?",
                                 "Да, обязательно. Всем самокатов! И Москве, и Московской области.",
                         },
@@ -55,10 +103,12 @@ public class ImportantQuestionsTest {
     }
 
     private WebDriver webDriver;
+    private final WebDriverFactory.BrowserType browserType;
     private final String question;
     private final String expectedAnswer;
 
-    public ImportantQuestionsTest(String question, String expectedAnswer) {
+    public ImportantQuestionsTest(WebDriverFactory.BrowserType browserType, String question, String expectedAnswer) {
+        this.browserType = browserType;
         this.question = question;
         this.expectedAnswer = expectedAnswer;
     }
@@ -68,20 +118,9 @@ public class ImportantQuestionsTest {
     }
 
     @Test
-    public void checkQuestionsFirefox() throws Exception {
-        webDriver = WebDriverFactory.getWebDriver(WebDriverFactory.BrowserType.FIREFOX);
+    public void checkQuestions() throws Exception {
+        webDriver = WebDriverFactory.getWebDriver(browserType);
         webDriver.get(MyConfiguration.PAGE_URL);
-        checkQuestions();
-    }
-
-    @Test
-    public void checkQuestionsChrome() throws Exception {
-        webDriver = WebDriverFactory.getWebDriver(WebDriverFactory.BrowserType.CHROME);
-        webDriver.get(MyConfiguration.PAGE_URL);
-        checkQuestions();
-    }
-
-    private void checkQuestions() {
         MainPage mainPage = new MainPage(webDriver);
         mainPage.clickConfirmCookieButton();
         String actualAnswer = mainPage.GetAnswer(question);
